65ac315ea454dea51bc7508bd991481e47e75796
CatiaAntunes <catiafantunes@gmail.com>
2024-05-19 19:32:20 +0100

    update alphabeta and variable names
    Added comments on alphabeta.py and changed variable names to be camel case

--- algorithms/minmax.py
+++ algorithms/minmax.py
@@ -16,33 +16,32 @@ def get_possible_moves(cells):
 1. The algorithm first checks if there's a winner or if the maximum depth (search limit) has been reached
 2. Depending on whose turn it is ('ai turn'), the function recursively evaluates all possible moves
 """
-
-def minmax(cells, depth, ai_turn):
+def minmax(cells, depth, aiTurn):
     winner = check_winner()
     if winner or depth == 0:
         return evaluate(winner)
 
     """ AI's Turn"""
-    if ai_turn:
-        # Initialize max_eval to negative infinity
-        max_eval = float('-inf')
+    if aiTurn:
+        # Initialize maxEval to negative infinity
+        maxEval = float('-inf')
         for cell in get_possible_moves(cells):
             # Simulate by clicking the cell with 'O'
             cell.click('O')
-            # Recursively call minmax with decreased depth and 'ai_turn' set to False
+            # Recursively call minmax with decreased depth and 'aiTurn' set to False
             eval = minmax(cells, depth - 1, False)
             # Undo the move by resetting the cell's clicked status and symbol
             cell.clicked = False
             cell.symbol = None
-            #Update max_eval with the maximum value between max_eval and the evaluation of the move
-            max_eval = max(max_eval, eval)
-        # Return max_eval    
-        return max_eval
+            #Update maxEval with the maximum value between maxEval and the evaluation of the move
+            maxEval = max(maxEval, eval)
+        # Return maxEval    
+        return maxEval
     
     # Minimizing Player's Turn
     else:
-        # Initialize 'min_eval' to positive infinity
-        min_eval = float('inf')
+        # Initialize 'minEval' to positive infinity
+        minEval = float('inf')
         # For each possible move
         for cell in get_possible_moves(cells):
             # Simulate the move by clicking the cell with 'X'
@@ -52,10 +51,10 @@ def minmax(cells, depth, ai_turn):
             # Undo the move by resetting the cell's clicked status and symbol
             cell.clicked = False
             cell.symbol = None
-            # Update min_eval with the minimum value between 'min_eval' and the evaluation of the move
-            min_eval = min(min_eval, eval)
-        # Return min_eval
-        return min_eval
+            # Update minEval with the minimum value between 'minEval' and the evaluation of the move
+            minEval = min(minEval, eval)
+        # Return minEval
+        return minEval
 
 """ Evaluate Function
 This function returns a score based on the winner
